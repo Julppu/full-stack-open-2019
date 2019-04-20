8dca69f85137c70c813f72bc05f627a4978860e0
Julppu <lindqvist.juha@gmail.com>
2019-04-20 22:26:07 +0300

    part 1.1: semicolons, typo

--- part1/src/index.js
+++ part1/src/index.js
@@ -2,19 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-    const course = 'Half Stack -sovelluskehitys'
-    const part1 = 'Reactin perusteet'
-    const exercises1 = 10
-    const part2 = 'Tiedonvälitys propseilla'
-    const exercises2 = 7
-    const part3 = 'Komponenttien tila'
-    const exercises3 = 14
+    const course = 'Half Stack -sovelluskehitys';
+    const part1 = 'Reactin perusteet';
+    const exercises1 = 10;
+    const part2 = 'Tiedonvälitys propseilla';
+    const exercises2 = 7;
+    const part3 = 'Komponenttien tila';
+    const exercises3 = 14;
 
     const Header = (props) => {
         return (
             <h1>{props.course}</h1>
         )
-    }
+    };
 
     const Content = (props) => {
         return (
@@ -22,7 +22,7 @@ const App = () => {
                 {props.part} {props.exercises}
             </p>
         )
-    }
+    };
 
     const Total = (props) => {
         return (
@@ -30,7 +30,7 @@ const App = () => {
                 yhteensä {props.total} tehtävää
             </p>
         )
-    }
+    };
 
     return (
         <div>
@@ -38,10 +38,9 @@ const App = () => {
             <Content part={part1} excercises={exercises1} />
             <Content part={part2} excercises={exercises2} />
             <Content part={part3} excercises={exercises3} />
-            <Content total={exercises1 + exercises2 + exercises3} />
-            <Total  />
+            <Total  total={exercises1 + exercises2 + exercises3} />
         </div>
     )
-}
+};
 
 ReactDOM.render(<App />, document.getElementById('root'))
