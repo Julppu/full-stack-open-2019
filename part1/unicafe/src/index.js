a59ba783271c360d1e4a24f4764bc96330d1bfb8
Julppu <lindqvist.juha@gmail.com>
2019-04-28 14:49:40 +0300

    part 1.6: unicafe step 1

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+    const [ good, setGood ] = useState(0);
+    const [ neutral, setNeutral ] = useState(0);
+    const [ bad, setBad ] = useState(0);
+
+    const setGoodValue = (value) => setGood(value);
+    const setNeutralValue = (value) => setNeutral(value);
+    const setBadValue = (value) => setBad(value);
+
+    return (
+        <div>
+            <h2>Anna palautetta</h2>
+            <Button handleClick={() => setGoodValue(good + 1)} text='hyvä'/>
+            <Button handleClick={() => setNeutralValue(neutral + 1)} text='neutraali'/>
+            <Button handleClick={() => setBadValue(bad + 1)} text='huono'/>
+            <Stats good={good} neutral={neutral} bad={bad}/>
+        </div>
+    )
+};
+
+const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>
+        {text}
+    </button>
+);
+
+const Stats = ({good, neutral, bad}) => {
+    return (
+        <div>
+            <h2>Statistiikka</h2>
+            <div>
+                <p>hyvä {good}</p>
+                <p>neutraali {neutral}</p>
+                <p>huono {bad}</p>
+            </div>
+        </div>
+    )
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
