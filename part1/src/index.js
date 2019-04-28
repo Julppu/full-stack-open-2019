504dcfa31abd37b040e3239eac8ddf7418dd4755
Julppu <lindqvist.juha@gmail.com>
2019-04-28 12:54:33 +0300

    move things outside App

--- part1/src/index.js
+++ part1/src/index.js
@@ -20,47 +20,47 @@ const App = () => {
         ]
     };
 
-    const Header = (props) => {
-        return (
-            <h1>{props.course.name}</h1>
-        )
-    };
-
-    const Part = (props) => {
-        return (
-            <p>{props.part.name} {props.part.exercises}</p>
-        )
-    };
-
-    const Content = (props) => {
-        return (
-            <div>
-                <Part part={props.parts[0]} />
-                <Part part={props.parts[1]} />
-                <Part part={props.parts[2]} />
-            </div>
-        )
-    };
-
-    const Total = (props) => {
-        let sum = 0;
-        props.parts.forEach( part => {
-            sum += part.exercises;
-        });
-
-        return (
-            <p>
-                yhteensä { sum } tehtävää
-            </p>
-        )
-    };
-
     return (
         <div>
             <Header course={course} />
             <Content parts={course.parts} />
             <Total parts={course.parts} />
         </div>
+    )
+};
+
+const Header = (props) => {
+    return (
+        <h1>{props.course.name}</h1>
+    )
+};
+
+const Part = (props) => {
+    return (
+        <p>{props.part.name} {props.part.exercises}</p>
+    )
+};
+
+const Content = (props) => {
+    return (
+        <div>
+            <Part part={props.parts[0]} />
+            <Part part={props.parts[1]} />
+            <Part part={props.parts[2]} />
+        </div>
+    )
+};
+
+const Total = (props) => {
+    let sum = 0;
+    props.parts.forEach( part => {
+        sum += part.exercises;
+    });
+
+    return (
+        <p>
+            yhteensä { sum } tehtävää
+        </p>
     )
 };
 
